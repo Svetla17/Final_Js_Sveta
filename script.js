7b5cc1ce61105576fb3e48cbc9821efc29646484
Svetla17 <SvetBar@gmail.com>
2021-11-28 13:53:03 +0300

    dip1

--- script.js
+++ script.js
@@ -27,19 +27,21 @@ contactBox.append(contactForm);
 let labFname = document.createElement('label');
 contactForm.append(labFname);
 labFname.className = 'label_form';
+labFname.setAttribute('name', 'labFname');
+labFname.setAttribute('for', 'contactFirstName');
 labFname.innerText = (' Имя : ');
 
 
 let contactFirstName = document.createElement('input');
+contactFirstName.setAttribute('id', 'contactFirstName');
 contactFirstName.className = 'contact_fname'
 contactForm.append(contactFirstName);
 contactFirstName.setAttribute('type', 'text');
 contactFirstName.setAttribute('placeholder', 'Имя пользователя');
 contactFirstName.setAttribute('pattern', '[a-zA-ZА-Яа-я]+');
 contactFirstName.required = true;
-// contactFirstName.setAttribute('autocomplete', 'on');
+// contactFirstName.setAttribute('value', '');
 contactFirstName.focus();
-
 
 let labLname = document.createElement('label');
 contactForm.append(labLname);
@@ -87,6 +89,13 @@ newBtn.className = 'finish_btn'
 document.body.prepend(newBtn);
 
 
+contactForm.addEventListener('submit', (event) => {                   
+    event.preventDefault();    
+    modal1.innerText = `${labFname.innerText}  ${contactFirstName.value}`;
+    modal2.innerText = `${labLname.innerText}  ${contactLastName.value}`;
+    modal3.innerText = `${labAge.innerText}  ${contactAge.value}`;
+});  
+    
 
 let person = localStorage.getItem('person'); 
 
@@ -96,6 +105,7 @@ if (person){
     newBtn.style.display = 'block';  
     finishBtn.textContent = `Продолжить как ${person}`;
     newBtn.textContent = `Ввести данные о себе снова`;
+    
 
     newBtn.addEventListener('click', () =>{          
         localStorage.clear();
@@ -110,19 +120,21 @@ if (person){
         finishBtn.style.display = 'none';
         newBtn.style.display = 'none'; 
         divQuestion.style.display = 'block'; 
+        
     })       
                                                      
 } else {
-    contactForm.addEventListener('submit', (event)=>{                   
-    event.preventDefault(); 
-    let user = `${contactFirstName.value} ${contactLastName.value}`;   
-    localStorage.setItem('person', user);   
-    divForm.style.display = 'none';       
-    contactForm.reset();
-    divQuestion.style.display = 'block';
-             
- }     
+    contactForm.addEventListener('submit', (event) => {                   
+        event.preventDefault(); 
+        let user = `${contactFirstName.value} ${contactLastName.value}`;   
+        localStorage.setItem('person', user);   
+        divForm.style.display = 'none';       
+        contactForm.reset();
+        divQuestion.style.display = 'block';        
+        
+    } 
 )};
+
 
 // Второй  блок - опросник.
 
@@ -147,7 +159,7 @@ const labInp1 = document.createElement('label');
 questionForm.append(labInp1);
 labInp1.className = 'label_form';
 labInp1.setAttribute('for', 'questionInp1');
-labInp1.innerText = (' Ваша профессия в настоящее время : ');
+labInp1.innerText = ('Ваша профессия в настоящее время: ');
 
 
 const questionInp1 = document.createElement('input');
@@ -164,7 +176,7 @@ const labInp2 = document.createElement('label');
 questionForm.append(labInp2);
 labInp2.className = 'label_form';
 labInp2.setAttribute('for', 'questionInp2');
-labInp2.innerText = (' Рассматриваемые вакансии : ');
+labInp2.innerText = (' Рассматриваемые вакансии: ');
 
 
 const questionInp2 = document.createElement('input');
@@ -191,51 +203,59 @@ questionInp3.setAttribute('type', 'range');
 questionInp3.setAttribute('min', '500');
 questionInp3.setAttribute('max', '5000');
 questionInp3.setAttribute('step', '50');
+questionInp3.setAttribute('value', '500') 
 questionInp3.setAttribute('oninput', 'num.value = this.value');
-questionInp3.required = true;
+
 
 let questionInp3_3 = document.createElement('output');
 questionInp3_3.setAttribute('id', 'num');
 questionInp3.after(questionInp3_3);
 questionInp3_3.setAttribute('for', 'questionInp3');
-questionInp3_3.innerText = questionInp3.value
+questionInp3_3.innerText = questionInp3.value;
+
 
 
 let fiel = document.createElement('fieldset');
 questionForm.append(fiel);
 let leg = document.createElement('legend');
 fiel.append(leg);
-leg.innerText = ' Предпочитаете работать (выбрать один или оба варианта ) : ';
+leg.innerText = ' Выбрать один или два варианта : ';
 
 
 const legBox1 = document.createElement('div');
 legBox1.className = 'legBox1';
 leg.after(legBox1);
+
 let questionInp4_y = document.createElement('input');
 questionInp4_y.className = 'question_Inp4'
 questionInp4_y.setAttribute('type', 'checkbox');
 questionInp4_y.setAttribute('id', 'questionInp4_y');
-questionInp4_y.setAttribute('value', 'в государственной компании');
+questionInp4_y.setAttribute('value', 'предложить варианты');
 legBox1.append(questionInp4_y);
+
 const lab_y = document.createElement('label');
 questionInp4_y.after(lab_y);
 lab_y.setAttribute('for', 'questionInp4_y');
-lab_y.innerText = ' в государственной компании ';
+lab_y.innerText = 'Работа в государственной компании -  ';
 
 
 const legBox2 = document.createElement('div');
 legBox2.className = 'legBox2';
 leg.after(legBox2);
+
 let questionInp4_n = document.createElement('input');
 questionInp4_n.className = 'question_Inp4'
 questionInp4_n.setAttribute('type', 'checkbox');
+questionInp4_n.setAttribute('checked', 'true');
 questionInp4_n.setAttribute('id', 'questionInp4_n');
-questionInp4_n.setAttribute('value', 'в частной компании');
+questionInp4_n.setAttribute('value', ' предложить варианты');
+
 legBox2.append(questionInp4_n);
 const lab_n = document.createElement('label');
 questionInp4_n.after(lab_n);
 lab_n.setAttribute('for', 'questionInp4_n');
-lab_n.innerText = ' в частной компании ';
+lab_n.innerText = 'Работа в частной компании - ';
+
 
 let selectHeader = document.createElement('div');
 fiel.after(selectHeader);
@@ -287,6 +307,7 @@ radioImp2.className = 'radioImp2'
 radioImp2.setAttribute('type', 'radio');
 radioImp2.setAttribute('id', 'radioImp2');
 radioImp2.setAttribute('name', 'radioImp');
+radioImp2.setAttribute('checked', 'true');
 radioImp2.setAttribute('value', ' от 1 до 3 лет ');
 labRad1.after(radioImp2);
 
@@ -315,9 +336,121 @@ questionBtn.className = 'question_btn'
 questionForm.append(questionBtn);
 questionBtn.innerText = 'Отправить';
 
+
 questionForm.addEventListener('submit', (event)=>{                   
     event.preventDefault(); 
+    modal1.innerText = `${labFname.innerText}  ${person}`;
+    modal2.innerText = `${labLname.innerText}  ${contactLastName.value}`;
+    modal3.innerText = `${labAge.innerText}  ${contactAge.value}`;
+    modal4.innerText = `${labInp1.innerText}  ${questionInp1.value}`;
+    modal5.innerText = `${labInp2.innerText}  ${questionInp2.value}`;
+    modal6.innerText = `${labInp3.innerText}  ${num.value}`;
+       
+    if (questionInp4_y.checked)  {
+        modal7.innerText = `${lab_y.innerText}  ${questionInp4_y.value}`;
+    } else {
+        modal7.innerText = `${lab_y.innerText}  не предлагать`;
+    };
+
+    if (questionInp4_n.checked)  {
+        modal8.innerText = `${lab_n.innerText}  ${questionInp4_n.value}`;
+    } else {
+        modal8.innerText = `${lab_n.innerText}  не предлагать`;
+    };  
+    modal9.innerText = `${selectHeader.innerText}  ${selectForm.value}`;
+
+    if (radioImp1.checked)  {
+    modal10.innerText = `${radioHeader.innerText}  ${radioImp1.value}`;
+    }
+    if (radioImp2.checked)  {
+        modal10.innerText = `${radioHeader.innerText}  ${radioImp2.value}`;
+        }
+    if (radioImp3.checked)  {
+    modal10.innerText = `${radioHeader.innerText}  ${radioImp3.value}`;
+    }
+
     questionForm.reset();
-             
- }     
-)
+    divQuestion.style.display = 'none';
+    modal.style.display = 'block';     
+         
+ });  
+ 
+ 
+// Третий  блок - модальное окно.
+
+const modal = document.createElement('div');
+modal.className ='modal';
+divQuestion.after(modal);
+
+const modalContent = document.createElement('div');
+modalContent.className = 'modal-content';
+modal.append(modalContent);
+
+const modalClose = document.createElement('span');
+modalClose.className = 'close';
+modalContent.append(modalClose);
+modalClose.innerText = '×'
+
+const modalHeader = document.createElement('h2');
+modalHeader.className = 'modal-header';
+modalClose.after(modalHeader);
+modalHeader.innerText = 'Поздравляем, Bы прошли опрос!'
+
+const modalResult = document.createElement('div');
+modalResult.className = 'modal-result';
+modalHeader.after(modalResult);
+
+const modal1 = document.createElement('p');
+modal1.className = 'modal_description';
+modalResult.append(modal1);
+
+const modal2 = document.createElement('p');
+modal2.className = 'modal_description';
+modalResult.append(modal2);
+
+const modal3 = document.createElement('p');
+modal3.className = 'modal_description';
+modalResult.append(modal3);
+
+const modal4 = document.createElement('p');
+modal4.className = 'modal_description';
+modalResult.append(modal4);
+
+const modal5 = document.createElement('p');
+modal5.className = 'modal_description';
+modalResult.append(modal5);
+
+const modal6 = document.createElement('p');
+modal6.className = 'modal_description';
+modalResult.append(modal6);
+
+const modal7 = document.createElement('p');
+modal7.className = 'modal_description';
+modalResult.append(modal7);
+
+const modal8 = document.createElement('p');
+modal8.className = 'modal_description';
+modalResult.append(modal8);
+
+const modal9 = document.createElement('p');
+modal9.className = 'modal_description';
+modalResult.append(modal9);
+
+const modal10 = document.createElement('p');
+modal10.className = 'modal_description';
+modalResult.append(modal10);
+
+const modalFooter = document.createElement('div');
+modalFooter.className = 'modal-footer';
+modalResult.after(modalFooter);
+
+const modalBtn = document.createElement('button');
+modalFooter.append(modalBtn);
+modalBtn.className = 'modal-btn';
+modalBtn.innerText = 'Нажмите на кнопку!';
+
+modalClose.onclick = function() {
+    modal.style.display = "none";
+}
+
+
